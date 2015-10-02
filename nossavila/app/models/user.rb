c57c8adc0c8b15a16d48fb4d64fcbb7d6088ddc2
luketis <luk.ties@gmail.com>
2015-10-02 19:53:57 +0000

    Factories added, Model spec in progress

--- nossavila/app/models/user.rb
+++ nossavila/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-    validates :first_name, lenght: {minimum: 2, maximum: 30}
-    validates :last_name, lenght: {minimum: 3, maximum: 30}
-    validates :cep, lenght: { is: 9 } #depois por a regex para verificar o formato
+    validates :first_name, lengt: {minimum: 2, maximum: 30}
+    validates :last_name, length: {minimum: 3, maximum: 30}
+    validates :cep, length: { is: 9 } #depois por a regex para verificar o formato
     # regex para verificar cep /\d{5}-?\d{3}/ - usuário pode entrar com cep ou sem hifen
     
     has_many :services
--- nossavila/spec/models/user_spec.rb
+++ nossavila/spec/models/user_spec.rb
@@ -1,20 +1,58 @@
 # /spec/models/user_spec.rb
 
 require 'spec_helper'
+require 'faker'
 
 describe User, :pending => true do
-    it "is invalid without a firstname"
-    it "is invalid if firstname lenght is less than 2 characteres"
-    it "is invalid without a lastname"
-    it "is invalid if lastname lenght is greater than 30 characteres"
-    it "is invalid without an adress"
-    it "is invalid without a cep"
-    it "is invalid if cep lenght is not eight"
-    it "is invalid without an email"
-    it "is invalid without a house number"
-    it "is invalid without cpf number"
-    it "should have a valid cpf number"
-    it "return user's full name as a string"
-    it "return cep as an integer"
-    it "has a valid email"
+    it "is invalid without a firstname" do
+        Factory.build(:user, first_name: nil).should_not be_valid
+    end
+    it "is invalid if firstname length is less than 2 characteres" do
+        Factory.build(:user, first_name: "A").should_not be_valid
+    end
+    it "is invalid without a lastname" do 
+        Factory.build(:user, last_name: nil).should_not be_valid
+    end
+    it "is invalid if lastname length is less than 3 characteres" do
+        Factory.build(:user, last_name: "Ivo").should_not be_valid
+    end
+    it "is invalid without an username" do
+        Factory.build(:user, username: nil).should_not be_valid
+    end
+    it "is invalid without a password" do
+        Factory.build(:user, password: nil).should_not be_valid
+    end
+    it "is invalid without an address" do 
+        Factory.build(:user, address: nil).should_not be_valid
+    end
+    it "is invalid without a cep" do 
+        Factory.build(:user, cep: nil).should_not be_valid
+    end
+    
+    it "is invalid if cep lenght is not eight" do
+        Factory.build(:user, cep: 131313).should_not be_valid
+    end
+    it "is invalid without an email" do
+        Factory.build(:user, email: nil).should_not be_valid
+    end
+    it "is invalid without a house number" do
+        Factory.build(:user, housenumber: nil).should_not be_valid
+    end
+    it "is invalid without cpf number" do
+        Factory.build(:user, cpf: nil).should_not be_valid
+    end
+    it "has a valid email" do
+        Factory.create(:user).is_valid_email?.should be_true
+    end
+    it "should have a valid cpf number" do
+        Factory.create(:user, cpf: 32442517817).is_valid_cpf?.should be_true
+    end
+    #métodos de instância
+    it "return user's full name as a string" do
+         Factory(:user, first_name: "John", last_name: "Thornton").name.should == "John Thornton"
+    end
+    it "return cep as an integer" do
+        Factory(:user, cep: 12345678).cep.should == 12345678
+    end
+    it ""
 end
