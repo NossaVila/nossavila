f14946af9820d75059d1a4e85d0a59fddf69721f
larissa13 <lariop13@gmail.com>
2015-10-05 18:02:20 +0000

    testes de validação e atribuição de 'service' adicionados

--- nossavila/spec/models/service_spec.rb
+++ nossavila/spec/models/service_spec.rb
@@ -3,9 +3,20 @@
 require 'spec_helper'
 
 describe Service, :pending => true do
-  it "is invalid without a title"
-  it "is invalid without a description"
+  it "is invalid without a title" do
+    Factory.build(:service, title: nil).should_not be_valid
+  end
+  it "is invalid without a description" do
+    Factory.build(:service, description: nil).should_not be_valid
+  end
   
   # validação de tags/categorias?
+  it "should set the title correctly" do 
+    expect(Factory.create(:service, title: "pintor-atendo a domicilio").title).to eq("pintor-atendo a domicilio")  
+  end 
+  it "should set the description correctly" do
+    expect(Factory.create(:service, description:"Tenha sua casa pintada por um renomado pintor do Jabaquara.").description).to eq("Tenha sua casa pintada por um renomado pintor do Jabaquara.")
+  end
   
 end
+ 
