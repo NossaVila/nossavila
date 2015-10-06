5eed4e85d5de3617632a709eca2e1821fa5ad778
fsfelix <felipescript@gmail.com>
2015-10-06 16:24:26 +0000

    Alterado o root para os paineis.

--- nossavila/config/routes.rb
+++ nossavila/config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   resources :panels
   resources :services
+  root 'services#index'
+  
+  
+  
+  
+  
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
