c6cdb3038a63cf889ece506a16d3180f09dee19d
fsfelix <felipescript@gmail.com>
2015-10-10 23:10:36 +0000

    rspec service controller pronto

--- nossavila/spec/controllers/services_controller_spec.rb
+++ nossavila/spec/controllers/services_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ServicesController do
 
 	let(:valid_attributes) {
-    {title: 'Artesanato', description: 'Belos trabalhos feitos artesanalmente'}
+    {title: 'Artesanato', description: 'Belos trabalhos feitos artesanalmente', user_id: '1'}
     }
     
     let(:invalid_attributes) { {title: nil, description: nil } }
@@ -15,7 +15,7 @@ describe ServicesController do
   describe "GET #index" do
     it "assigns all services as @services" do
       service = Service.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {}
       expect(assigns(:services)).to eq([service])
     end
   end
@@ -24,15 +24,15 @@ describe ServicesController do
   describe "GET #show" do
     it "assigns the requested service  as @service" do
       service = Service.create! valid_attributes
-      get :show, {}, valid_session
-      expect(assigns(:services)).to eq(service)
+      get :show, {:id => service.to_param}, valid_session
+      expect(assigns(:service)).to eq(service)
     end
   end
 
   describe "GET #new" do
     it "assigns a new service as @service" do
       service = Service.create! valid_attributes
-      get :new, {}, valid_session
+      get :new, {:id => service.to_param}, valid_session
       expect(assigns(:services)).to eq([service])
       expect(assigns(:service)).to be_a_new(Service)
     end
@@ -42,16 +42,17 @@ describe ServicesController do
     let(:services) { [FactoryGirl.build(:service)] }
 
     before :each do
-      get :new, {}, valid_session
+      get :new, {}
     end
-
-    it { is_expected.to render_template(:new) }
+    it 'redirect to new_user_session_path if user is not logged in' do
+     expect(response).to redirect_to(new_user_session_path)
+    end
   end
   
   describe "GET #edit" do
     it "assigns the requested service as @service" do
       service = Service.create! valid_attributes
-      get :edit, {:service => valid_attributes}, valid_session
+      get :edit, {:id => service.to_param}
       expect(assigns(:service)).to eq(service)
     end
   end
@@ -60,30 +61,30 @@ describe ServicesController do
     context "with valid params" do
       it "creates a new service" do
         expect {
-          post :create, {:service => valid_attributes}, valid_session
+          post :create, {:service => valid_attributes }
         }.to change(Service, :count).by(1)
       end
 
       it "assigns a newly created service as @service" do
-        post :create, {:service => valid_attributes}, valid_session
+        post :create, {:service => valid_attributes}
         expect(assigns(:service)).to be_a(Service)
         expect(assigns(:service)).to be_persisted
       end
 
       it "redirects to the created service" do
-        post :create, {:service => valid_attributes}, valid_session
+        post :create, {:service => valid_attributes}
         expect(response).to redirect_to(Service.last)
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved service as @service" do
-        post :create, {:service => invalid_attributes}, valid_session
+        post :create, {:service => invalid_attributes}
         expect(assigns(:service)).to be_a_new(Service)
       end
 
       it "renders the 'new' template" do
-        post :create, {:service => invalid_attributes}, valid_session
+        post :create, {:service => invalid_attributes} 
         expect(response).to render_template("new")
       end
     end
@@ -97,19 +98,19 @@ describe ServicesController do
 
       it "updates the requested service" do
         service = Service.create! valid_attributes
-        put :update, {:id => service.to_param, :service => new_attributes}, valid_session
+        put :update, {:id => service.to_param, :service => new_attributes}
         service.reload
       end
 
       it "assigns the requested service as @service" do
         service = Service.create! valid_attributes
-        put :update, {:id => service.to_param, :service => valid_attributes}, valid_session
+        put :update, {:id => service.to_param, :service => valid_attributes}
         expect(assigns(:service)).to eq(service)
       end
 
       it "redirects to the service" do
         service = Service.create! valid_attributes
-        put :update, {:id => service.to_param, :service => valid_attributes}, valid_session
+        put :update, {:id => service.to_param, :service => valid_attributes}
         expect(response).to redirect_to(service)
       end
     end
@@ -117,13 +118,13 @@ describe ServicesController do
     context "with invalid params" do
       it "assigns the service as @service" do
         service = Service.create! valid_attributes
-        put :update, {:id => service.to_param, :service => invalid_attributes}, valid_session
+        put :update, {:id => service.to_param, :service => invalid_attributes}
         expect(assigns(:service)).to eq(service)
       end
 
       it "renders the 'edit' template" do
         service = Service.create! valid_attributes
-        put :update, {:id => service.to_param, :service => invalid_attributes}, valid_session
+        put :update, {:id => service.to_param, :service => invalid_attributes}
         expect(response).to render_template("edit")
       end
     end
@@ -133,16 +134,15 @@ describe ServicesController do
     it "destroys the requested service" do
       service = Service.create! valid_attributes
       expect {
-        delete :destroy, {:id => service.to_param}, valid_session
+        delete :destroy, {:id => service.to_param}
       }.to change(Service, :count).by(-1)
     end
 
     it "redirects to the services list" do
       service = Service.create! valid_attributes
-      delete :destroy, {:id => service.to_param}, valid_session
+      delete :destroy, {:id => service.to_param}
       expect(response).to redirect_to(services_url)
     end
   end
 
 end
-
